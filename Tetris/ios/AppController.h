1571598969d22f7ddcdf754beebdb17b639cbe98
zhou han <han.zhou@gu3.co.jp>
2013-10-29 22:11:22 +0900

    Initial Commit

---
+++ Tetris/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TetrisAppController.h
+//  Tetris
+//
+//  Created by hanks on 2013/10/29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Tetris/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TetrisAppController.h
+//  Tetris
+//
+//  Created by hanks on 2013/10/29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
